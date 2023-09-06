36d344cc08900ce5d246e8300a2ebb665c665bdb
Milan Singh <milansingh163@gmail.com>
2023-09-07 00:48:35 +0530

    logUtility.py fixed

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -11,13 +11,14 @@ class Test_001_login:
     base_url = ReadConfig.get_base_url()
     username = ReadConfig.get_username()
     password = ReadConfig.get_password()
-    logger = LogGeneration.loggen()
+    log_message = LogGeneration.loggen()
+
     def test_homepagetitle(self,setup):
-        self.logger.info("Test_001_login")
+        self.log_message.info('***********Home page test started**********')
         self.driver = setup
         self.driver.get(self.base_url)
         title = self.driver.title
-        self.logger.info("Title accessed")
+        self.log_message.info('***********title accessed**********')
         if title=='Your store. Login':
             self.driver.close()
             assert True
--- utilities/logUtility.py
+++ utilities/logUtility.py
@@ -1,13 +1,23 @@
 import logging
-
+import os
+from datetime import datetime
 class LogGeneration:
     @staticmethod
     def loggen():
-        logging.basicConfig(
-            filename=".Logs/automation.log",
-            format= "%(asctime)s [%(levelname)s] - %(message)s",
-            datefmt= "%Y-%m-%d %H:%M:%S"
-        )
+        # logging.basicConfig(
+        #     filename=".\\Logs\\automation.log",
+        #     filemode='w',
+        #     format="%(asctime)s: %(levelname)s: %(message)s",
+        #     datefmt="%Y-%m-%d %H:%M:%S",
+        # )
+        # logger = logging.getLogger()
+        # logger.setLevel(logging.DEBUG)
+        
+        # new method is working for me 
         logger = logging.getLogger()
+        fhandler = logging.FileHandler(filename='.\\Logs\\automation.log', mode='a')
+        formatter = logging.Formatter('%(asctime)s -  %(levelname)s - %(message)s')
+        fhandler.setFormatter(formatter)
+        logger.addHandler(fhandler)
         logger.setLevel(logging.INFO)
         return logger
